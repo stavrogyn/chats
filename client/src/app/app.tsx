f7e5827567ebc3f7d053ddd3c0bf1c33b8483509
stavrogyn <nikitakrugovoy@gmail.com>
2022-12-25 00:58:25 +0800

    chore: remove create react app + move client to vite

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,3 +1,17 @@
+import { useState } from 'react';
+
 export function App() {
-  return <div className="App">Hello</div>;
+  const [state, setState] = useState({ count: 0 });
+  const { count } = state;
+
+  const increment = () => {
+    setState({ count: count + 1 });
+  };
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
 }
